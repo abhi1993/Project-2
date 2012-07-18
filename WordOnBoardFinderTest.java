3cef1b56c1db1bcb5f4f1e6f64fa408c7b844c9c
abhijitpujare <abhijitpujare@gmail.com>
2012-07-18 13:23:13 -0700

    Update wordsOnList

--- WordOnBoardFinderTest.java
+++ WordOnBoardFinderTest.java
@@ -46,11 +46,64 @@ public class WordOnBoardFinderTest extends TestCase {
     BoggleBoard board = myMaker.makeBoard(boardContents);
 
     String[] cornerWords = { "ate", "noted", "net", "urn", "bind", "aside",
-                            "noise", "quack", "armin" };
+                            "noise","quack","armin"  };
     for (String s : cornerWords) {
       List<BoardCell> list = myFinder.cellsForWord(board, s);
       String word = getWord(board, list);
       assertEquals("fail for " + s, s, word);
     }
   }
+  
+  /*
+   * Tests that no cell is used twice when verifying that a word is in the board.
+   * Also tests words that do appear in the board.
+   */
+  
+  public void testRepeatingLetters(){
+    String[] boardContents = { "abwrj", "sojrt", "luqie", "teotl", "armin" };
+	    BoggleBoard board = myMaker.makeBoard(boardContents);
+
+	    String[] invalidWords = { "lul","mill","tell"};
+	    for (String s : invalidWords) {
+	      List<BoardCell> list = myFinder.cellsForWord(board, s);
+	      String word = getWord(board, list);
+	      assertEquals("", word);
+	    }
+	    
+	    
+
+	    String[] validWords = { "absolute","term","quilt","lute"};
+	    for (String s : validWords) {
+	      List<BoardCell> list = myFinder.cellsForWord(board, s);
+	      String word = getWord(board, list);
+	      assertEquals("fail for " + s, s, word);
+	    }
+  }
+ 
+  /*
+   * Tests that words that are formed by non-adjacent cells are not verified. 
+   * Also tests words that do appear in the board. 
+   */
+  
+  public void testInvalidWords(){
+	  String[] boardContents = { "ratqw", "sojrt", "pirws", "aqerd", "peudw" };
+	    BoggleBoard board = myMaker.makeBoard(boardContents);
+
+	    String[] invalidWords = { "sort","rats","reds","piss"};
+	    for (String s : invalidWords) {
+	      List<BoardCell> list = myFinder.cellsForWord(board, s);
+	      String word = getWord(board, list);
+	      assertEquals("", word);
+	    }
+	    
+	    String[] validWords = { "rat","sat","raj","queue","pore"};
+	    for (String s : validWords) {
+	      List<BoardCell> list = myFinder.cellsForWord(board, s);
+	      String word = getWord(board, list);
+	      assertEquals("fail for " + s, s, word);
+	    }
+	  
+  }
+  
+  
 }
